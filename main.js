b8e2ae89f7eed850589ad328e93243d146b68ccf
perecherick <sadpmhtml@gmail.com>
2023-03-05 23:08:56 +0200

    test

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var $=function(id){
+	return document.getElementById(id);
+}
+
 function change() {
     const hero = document.getElementById('hero');
     const item = document.getElementById('item');
@@ -18,3 +22,44 @@ function change() {
       document.getElementById("allitems").style.display="none";
   }
 }
+
+const detail_img = $('detail');
+const full_hero = $('full_hero');
+
+for(let element of document.getElementsByClassName('hero_strength'))
+  element.onclick = function(){
+    detail_img.src = this.src;
+    full_hero.classList.add('full_hero_visible');
+}
+
+for(let element of document.getElementsByClassName('hero_agility'))
+  element.onclick = function(){
+    detail_img.src = this.src;
+    full_hero.classList.add('full_hero_visible');
+}
+
+for(let element of document.getElementsByClassName('hero_intelegense'))
+  element.onclick = function(){
+    detail_img.src = this.src;
+    full_hero.classList.add('full_hero_visible');
+}
+
+function hide(){
+  $('full_hero').classList.add('full_hero_invisible');
+  $('full_hero').classList.remove('full_hero_visible');
+}
+
+let heroStrengths = document.querySelectorAll('.hero_strength');
+
+// Перебрать все элементы с классом 'hero_strength'
+heroStrengths.forEach(function(heroStrength) {
+  // Добавить обработчик событий 'click'
+  heroStrength.addEventListener('click', function() {
+    // Получить текст, который находится под картинкой
+    let heroName = heroStrength.nextElementSibling.innerText.trim();
+    // Поместить текст в элемент с id 'hero_name'
+    document.querySelector('#hero_name').innerText = alert(heroName);
+  });
+});
+
+$('full_hero').innerText = heroName;
