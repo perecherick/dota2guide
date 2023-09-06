ea16fac741cfe91183c6d576599279f5f7474956
perecherick <sadpmhtml@gmail.com>
2023-09-06 23:19:05 +0200

    Big update

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ function change() {
 
 const detail_img = $('detail');
 const full_hero = $('full_hero');
+const skill = $('skill');
 
 function changeHeroItemsSet () {
   if(document.getElementsByClassName('recomended_items')[0].style.display == "none"){
@@ -56,36 +57,42 @@ function changeHeroItemsSet () {
   }
 }
 
-function uploadHeroSkillBuild (hero_name){ // скилл билд
-  let imageSkills = getSkillsImageSourcesArray(hero_name);
-  for(let imgsrc of imageSkills)
-    $("skill_build").insertAdjacentHTML("beforeend", `
-      <img src="${linkStart + imgsrc}" class="added_skills">
-    `);
-}
 
+/* $("skill").onclick = function() {
+  let skill_link = event.target.getAttribute("skill");
+  
+  uploadHeroDescription(skill_link);
+  } */
+  
 let heroes = document.getElementsByClassName('hero');
 
 for (let el of heroes)
-  el.onclick = function() {
+el.onclick = function() {
+  
+  let hero_name = this.nextElementSibling.innerText;
 
-    let hero_name = this.nextElementSibling.innerText;
-
-    detail_img.src = this.src;
-    $('hero_name').innerText = hero_name;
-    full_hero.classList.add('full_hero_visible');
-    full_hero.classList.remove('full_hero_invisible');
-
+    // upload data
     uploadHeroDescription(hero_name);
     uploadHeroItems(hero_name);
     uploadHeroItems2(hero_name);
     uploadHeroSkillBuild(hero_name);
+    detail_img.src = this.src;
+    $('hero_name').innerText = hero_name;
+    
+    // show window
+    full_hero.classList.add('full_hero_visible');
+    full_hero.classList.remove('full_hero_invisible');
+    
+    // set up listeners on the skill images
+    setUpClickOnHeroSkills($("skill_build").getElementsByClassName("added_skills"));
 }
+
+
 
 function uploadHeroItems (hero_name) {
   let imageSources = getItemImageSourcesArray(hero_name);
   let itemContainers = $("recomended_items").getElementsByClassName("hero_item_container");
-
+  
   for (let a = 0; a < imageSources.length; a++) {
     for(let b = 0; b < imageSources[a].length; b++){
       let img = document.createElement("img");
@@ -99,7 +106,7 @@ function uploadHeroItems (hero_name) {
 function uploadHeroItems2 (hero_name) {
   let imageSources2 = getItemImageSourcesArray2(hero_name);
   let itemContainers2 = $("recomended_items_2").getElementsByClassName("hero_item_container");
-
+  
   for (let a = 0; a < imageSources2.length; a++) {
     for(let b = 0; b < imageSources2[a].length; b++){
       let img = document.createElement("img");
@@ -108,12 +115,61 @@ function uploadHeroItems2 (hero_name) {
       itemContainers2[a].appendChild(img);
     }
   }
-} 
+}
+
+function uploadHeroSkillBuild (hero_name){ // скилл билд
+  let imageSkills = getSkillsImageSourcesArray(hero_name);
+  for(let imgsrc of imageSkills)
+    $("skill_build").insertAdjacentHTML("beforeend", `
+      <img src="${linkStart + imgsrc}" class="added_skills" skill_name="${getSkillName(imgsrc)}">
+    `);
+}
+
+function getSkillName(imgSrc) {
+  var finalName = [];
+  var allowCopy = false;
+
+  for (let i = imgSrc.length - 1; i >= 0; i--) {
+
+    if (imgSrc[i] == ".") {
+      allowCopy = !allowCopy;
+      continue;
+    } else if (imgSrc[i] == "/") break;
+
+    if (allowCopy) {
+      finalName[finalName.length] = imgSrc[i];
+    }
+  }
+  
+  finalName.reverse();
+  return finalName.join('');
+}
+
+function setUpClickOnHeroSkills(skillEls) {
+
+  for (let i = 0; i < skillEls.length - 1; i++) {
+    skillEls[i].onclick = function () {
+      openSkillInfoWindow(this);
+    }
+  }
+}
+
+function openSkillInfoWindow(skillEl) {
+  // upload img
+  $("skill_img").src = skillEl.src;
+
+  // upload description
+  $("skill_description").innerText = uploadSkillDescription(skillEl.getAttribute("skill_name"));
+
+  // show window
+  $("skill_info_window").classList.add('skill_deatils_visible');
+  $("skill_info_window").classList.remove('skill_deatils');
+}
 
 $('drag').onclick = function() {
   var heroDescription = $("hero_description");
   heroDescription.textContent = "";
-
+  
   $("skill_build").innerHTML = "";
   
   let itemContainers = $("recomended_items").getElementsByClassName("hero_item_container");
