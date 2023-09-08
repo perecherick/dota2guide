d24d4196606a6c46ac793a0e0991e0ec3cc3b4c9
perecherick <sadpmhtml@gmail.com>
2023-09-08 15:56:06 +0200

    Added skill details

--- main.js
+++ main.js
@@ -71,50 +71,45 @@ el.onclick = function() {
   
   let hero_name = this.nextElementSibling.innerText;
 
-    // upload data
-    uploadHeroDescription(hero_name);
-    uploadHeroItems(hero_name);
-    uploadHeroItems2(hero_name);
-    uploadHeroSkillBuild(hero_name);
-    detail_img.src = this.src;
-    $('hero_name').innerText = hero_name;
-    
-    // show window
-    full_hero.classList.add('full_hero_visible');
-    full_hero.classList.remove('full_hero_invisible');
-    
-    // set up listeners on the skill images
-    setUpClickOnHeroSkills($("skill_build").getElementsByClassName("added_skills"));
-}
+  // upload data
+  uploadHeroDescription(hero_name);
+  uploadHeroItems(
+    getItemImageSourcesArray(hero_name),
+    $("recomended_items").getElementsByClassName("hero_item_container")
+  );
+  uploadHeroItems(
+    getItemImageSourcesArray2(hero_name),
+    $("recomended_items_2").getElementsByClassName("hero_item_container")
+  );
+  uploadHeroSkillBuild(hero_name);
+  detail_img.src = this.src;
+  $('hero_name').innerText = hero_name;
 
-
-
-function uploadHeroItems (hero_name) {
-  let imageSources = getItemImageSourcesArray(hero_name);
-  let itemContainers = $("recomended_items").getElementsByClassName("hero_item_container");
+  // set up draging on line button
+  fullHero.addEventListener('mousedown', function(e) {
+    startDragging(e);
+  });
+  fullHero.addEventListener('mousedown', function(e) {
+    startDragging(e);
+  });
   
-  for (let a = 0; a < imageSources.length; a++) {
+  // show window
+  full_hero.classList.add('full_hero_visible');
+  full_hero.classList.remove('full_hero_invisible');
+  
+  // set up listeners on the skill images
+  setUpClickOnHeroSkills($("skill_build").getElementsByClassName("added_skills"));
+} 
+
+function uploadHeroItems (imageSources, itemContainers) {
+
+  for (let a = 0; a < imageSources.length; a++)
     for(let b = 0; b < imageSources[a].length; b++){
       let img = document.createElement("img");
       img.src = linkStart + imageSources[a][b];
       img.classList.add("added_items");
       itemContainers[a].appendChild(img);
     }
-  }
-} 
-
-function uploadHeroItems2 (hero_name) {
-  let imageSources2 = getItemImageSourcesArray2(hero_name);
-  let itemContainers2 = $("recomended_items_2").getElementsByClassName("hero_item_container");
-  
-  for (let a = 0; a < imageSources2.length; a++) {
-    for(let b = 0; b < imageSources2[a].length; b++){
-      let img = document.createElement("img");
-      img.src = linkStart + imageSources2[a][b];
-      img.classList.add("added_items");
-      itemContainers2[a].appendChild(img);
-    }
-  }
 }
 
 function uploadHeroSkillBuild (hero_name){ // скилл билд
@@ -162,8 +157,8 @@ function openSkillInfoWindow(skillEl) {
   $("skill_description").innerText = uploadSkillDescription(skillEl.getAttribute("skill_name"));
 
   // show window
-  $("skill_info_window").classList.add('skill_deatils_visible');
-  $("skill_info_window").classList.remove('skill_deatils');
+  $("skill_info_window").classList.add('skill_details_visible');
+  $("skill_info_window").classList.remove('skill_details');
 }
 
 $('drag').onclick = function() {
@@ -196,9 +191,6 @@ let divfullHero = $('full_hero');
 let startY = 15;
 let isDragging = false;
 let newTop = 0;
-
-fullHero.addEventListener('mousedown', startDragging);
-fullHero.addEventListener('touchstart', startDragging);
 
 function startDragging(e) {
   if (e.target.id === 'drag') {
@@ -264,5 +256,94 @@ function dragElement(e) {
 
 // Проверяем, если пользователь зашел с мобильного устройства
 function isMobileDevice() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+$('drag2').onclick = function() {
+  var skillDescription = $("skill_description");
+  skillDescription.textContent = "";
+  
+  $('skill_info_window').style.top = "100%"; 
+  
+  $('skill_info_window').classList.add('skill_details');
+
+  setTimeout(() => {
+    $('skill_info_window').classList.remove('skill_details_visible');
+    $('skill_info_window').style.top = "0"; 
+  }, 300);
+}
+
+let fullHero2 = $('skill_info_window');
+let startY2 = 0;
+let isDragging2 = false;
+let newTop2 = 0;
+
+fullHero2.addEventListener('mousedown', startDragging2);
+fullHero2.addEventListener('touchstart', startDragging2);
+
+fullHero2.addEventListener('mousedown', startDragging2);
+fullHero2.addEventListener('touchstart', startDragging2);
+
+function startDragging2(e) {
+  if (e.target.classList.contains('drag')) {
+    isDragging2 = true;
+    startY2 = e.clientY || e.touches[0].clientY;
+    e.target.style.cursor = 'grabbing';
+    document.addEventListener('mousemove', dragElement2);
+    document.addEventListener('touchmove', dragElement2);
+    document.addEventListener('mouseup', stopDragging2);
+    document.addEventListener('touchend', stopDragging2);
+  }
+}
+
+function stopDragging2() {
+  isDragging2 = false;
+  $('drag2').style.cursor = 'grab';
+  fullHero2.classList.add('full_skill_transition');
+  
+  if (newTop2 >= Number(fullHero2.clientHeight) * 0.5) {
+    $('drag2').click();
+  } else {
+    fullHero2.style.top = "0";
+  }
+  
+  // Удаляем обработчики событий, чтобы перестать перетаскивать элемент
+  document.removeEventListener('mousemove', dragElement2);
+  document.removeEventListener('touchmove', dragElement2);
+  document.removeEventListener('mouseup', stopDragging2);
+  document.removeEventListener('touchend', stopDragging2);
+  setTimeout(()=>{
+    fullHero2.classList.remove("full_skill_transition");
+  },300);
+}
+
+function dragElement2(e) {
+  if (!isDragging2) return;
+  let newY2 = 0;
+  if(isMobileDevice2()) newY2 = e.touches[0].clientY;
+  else newY2 = e.clientY;
+  const deltaY2 = newY2 - startY2;
+  const currentTop2 = parseInt(window.getComputedStyle($("drag2")).top, 10)
+  newTop2 = currentTop2 + deltaY2;
+
+  // Проверяем, если новая позиция y меньше нуля, устанавливаем y-координату равной нулю
+  if (newTop2 < 0) {
+    fullHero2.style.top = "0";
+  } else {
+    fullHero2.style.top = newTop2 + "px";
+  }
+
+  // Проверяем, если див достиг конца экрана -75 по y-координате, выполняем функцию back
+  if (newTop2 > Number(fullHero2.clientHeight) - 75) {
+    $('drag2').click();
+    stopDragging2();
+  }
+  
+
+  // Используем preventDefault(), чтобы предотвратить прокрутку страницы при касании на мобильных устройствах
+  e.preventDefault();  
+}
+
+function isMobileDevice2() {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
